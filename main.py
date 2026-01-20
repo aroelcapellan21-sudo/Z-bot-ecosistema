47ed92e190b175c06228b9926d082d043002d886
aroelcapellan21-sudo <aroelcapellan21@gmail.com>
2026-01-19 22:22:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,18 +10,18 @@ def home():
     return "Z-Bot Vivo"
 
 def run():
-    # Linea corta para que no se corte en tu celular
-    p = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=p)
+    puerto = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=puerto)
 
 def keep_alive():
     Thread(target=run).start()
 
-TOKEN = "7547196020:AAFvjQ63B2C9v_78524Iu-8N68B8lH6M_9M"
+# PEGA EL NUEVO TOKEN DENTRO DE LAS COMILLAS
+TOKEN = "7991523120:AAFNbwFDhtpm0ziyNfBDLs9KXrB_r3Fs110"
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start', 'socia'])
-def send_welcome(message):
+def welcome(message):
     bot.reply_to(message, "¡Z-Bot Despierta! Activo para Yayo y Socia.")
 
 if __name__ == "__main__":

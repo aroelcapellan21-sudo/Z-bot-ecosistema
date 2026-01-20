8a1c082aeb5200668c4f89aca637d144ffeb4951
aroelcapellan21-sudo <aroelcapellan21@gmail.com>
2026-01-19 20:59:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ from flask import Flask
 from threading import Thread
 import telebot
 
-# Servidor para mantener a Render despierto
 app = Flask('')
 
 @app.route('/')
 def home():
-    return "Z-Bot está despierto"
+    return "Z-Bot Vivo"
 
 def run():
     app.run(host='0.0.0.0', port=10000)
@@ -17,19 +16,14 @@ def keep_alive():
     t = Thread(target=run)
     t.start()
 
-# --- TUS TOKENS CONFIGURADOS ---
 TOKEN = "7547196020:AAFvjQ63B2C9v_78524Iu-8N68B8lH6M_9M"
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['start', 'socia'])
 def send_welcome(message):
-    bot.reply_to(message, "¡Z-Bot Despierta! Estoy activo y a tus órdenes, Yayo.")
+    bot.reply_to(message, "¡Z-Bot Despierta! Estoy activo y a tus órdenes, Yayo y Socia.")
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, "Recibido. Estamos probando la estabilidad antes de la lógica de trading.")
-
-if __name__ == "__main__":
+if name == "main":
     keep_alive()
-    print("Bot encendido...")
+    print("Bot encendido correctamente...")
     bot.infinity_polling()

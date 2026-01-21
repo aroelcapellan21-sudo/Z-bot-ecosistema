fbb7484265edf3bd845f01fda544b0f009d196e1
aroelcapellan21-sudo <aroelcapellan21@gmail.com>
2026-01-21 10:18:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,27 @@ from flask import Flask
 from threading import Thread
 
 app = Flask('')
+
+# Tu ID para reportes automáticos
+CHAT_ID = "6578945006"
+
+# Esta línea busca el secreto que guardaste en Render
+TOKEN = os.environ.get("TELEGRAM_TOKEN")
+bot = telebot.TeleBot(TOKEN)
+
 @app.route('/')
-def home(): return "Z-Bot Activo"
+def home():
+    # El bot te avisa cada vez que el Cron-job lo visita
+    try:
+        bot.send_message(CHAT_ID, "🦾 Reporte de Guardia: El Padre está vigilando el ecosistema.")
+    except Exception as e:
+        print(f"Error: {e}")
+    return "Z-Bot Activo"
 
 def run():
     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
 
 Thread(target=run).start()
-
-# Esta línea busca el secreto que guardaste en Render
-TOKEN = os.environ.get("TELEGRAM_TOKEN")
-bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start', 'socia'])
 def welcome(message):
